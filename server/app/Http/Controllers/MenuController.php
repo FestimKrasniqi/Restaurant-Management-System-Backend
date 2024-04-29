1685acf9423680f57aef00b01f03ae234b1a9a63
Festim <krasniqif937@gmail.com>
2024-04-29 14:56:12 +0200

    Implemented Gate functionality for Menu Authorization by admin and also implemented update and delete API routes and their logic in Menu Controller

--- server/app/Http/Controllers/MenuController.php
+++ server/app/Http/Controllers/MenuController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Menu;
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Storage;
+
 
 class MenuController {
 
     function  insertMenu (Request $req) {
 
-        
     
         $validate = Validator::make($req->all(), [
          'name' => 'required|string|max:255',
@@ -30,6 +33,11 @@ class MenuController {
             ],422);
         }
 
+        if (!Gate::allows('manage-menu')) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+       
+
         $category = Category::firstOrCreate(['category_name'=> $req->category_name]);
 
         $menu = Menu::create([
@@ -39,22 +47,18 @@ class MenuController {
             'image_url' => $req->file('image_url')->store('products')
         ]);
 
-
-        
         $menu->category()->associate($category);
        
 
         $menu->save();
 
-        $menusInCategory = $category->menus;
-
         return response()->json([
-            'message' => 'Menu item created successfully',
-            'menus_in_category' => $menusInCategory
+            'message' => 'Menu item created successfully'
         ],200);
 
         
     }
+
 
     function getMenuByCategory(Request $req,$categoryName) {
 
@@ -69,11 +73,69 @@ class MenuController {
      return response()->json($menus,200);
     }
 
+
     function allMenu(Request $req) {
         $menus = Menu::with('category')->get();
 
         return response()->json($menus,200);
     }
+
+
+    function updateMenu (Request $req, $id) {
+
+        $validator = Validator::make($req->all(), [
+            'name' => 'sometimes|string|max:255',
+            'description' => 'sometimes|string',
+            'price' => 'sometimes|numeric',
+            'image_url' => 'sometimes|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'category_name' => 'sometimes|string'
+        ]);
+    
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => $validator->errors()->first(),
+                'status' => false
+            ], 422);
+        }
+    
+        
+        $menu = Menu::findOrFail($id);
+
+        if (!Gate::allows('manage-menu')) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        
+    
+       
+        $menu->update([
+            'name' => $req->name ?? $menu->name,
+            'price' => $req->price ?? $menu->price,
+            'description' => $req->description ?? $menu->description,
+            'image_url' => $req->file('image_url') ? $req->file('image_url')->store('products') : $menu->image_url
+        ]);
+    
+    
+        if ($req->has('category_name')) {
+            $menu->category()->update(['category_name' => $req->category_name]);
+        }
+    
+        return response()->json(["message" => "Menu updated successfully","menu"=>$menu], 200);
+    }
+
+
+    public function destroy(Request $req, $id) {
+        $menu = Menu::findOrFail($id);
+
+        if (!Gate::allows('manage-menu')) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $menu->delete();
+
+        return response()->json(["message"=>"Menu deleted successfully"],200);
+
+    }
+
 }
 
 ?>
